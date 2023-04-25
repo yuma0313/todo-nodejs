174fd433f1e8c5ff8a8d95cc0e05ec34240a1aee
yuma0313 <y.yuma0313@gmail.com>
2023-04-25 18:38:41 +0900

    編集機能作成

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ app.use(bodyParser.json());
 // EJSの設定
 app.set("view engine", "ejs");
 
-//トップページへのルーティング
-app.get("/", function (req, res) {
-  res.render("index", { tasks: tasks });
-});
-
 //サーバーを開始する
 app.listen(3000, function () {});
 
@@ -23,31 +18,40 @@ const tasks = [
   { id: 3, title: "test3" },
 ];
 
-//タスク登録へのルーティング
-app.post("/create-task", function (req, res) {
-  let task = {};
-  //タスクが未登録の場合1を登録
-  if (tasks.length === 0) {
-    task = {
-      id: 1,
-      title: req.body.title,
-    };
-  } else {
-    //タスクが存在する場合最後の要素を取得
-    const lastElement = tasks[tasks.length - 1];
-    //最後の要素のidを取得
-    const lastId = lastElement.id;
-    task = {
-      id: lastId + 1,
-      title: req.body.title,
-    };
-  }
-
-  tasks.push(task);
+//トップページのルーティング
+app.get("/", function (req, res) {
   res.render("index", { tasks: tasks });
 });
 
-//完了機能
+//新しいidを生成する関数
+const generateTaskId = () => {
+  //タスクが存在しない場合id:1を返す
+  if (tasks.length === 0) {
+    return 1;
+  } else {
+    //タスクが存在する場合最後の要素のインデックスを取得
+    const lastElement = tasks[tasks.length - 1];
+    //最後の要素のidを取得
+    const lastId = lastElement.id;
+
+    return lastId + 1;
+  }
+};
+
+//タスク登録処理
+app.post("/create", function (req, res) {
+  let task = {};
+
+  task = {
+    id: generateTaskId(),
+    title: req.body.title,
+  };
+
+  tasks.push(task);
+  res.redirect("/");
+});
+
+//タスク完了処理
 app.post("/complete-task", (req, res) => {
   const taskId = req.body.taskId;
 
@@ -56,19 +60,35 @@ app.post("/complete-task", (req, res) => {
   //インデックスの要素を削除
   tasks.splice(index, 1);
 
-  res.render("index", { tasks: tasks });
+  res.redirect("/");
 });
 
-//編集機能
-app.use("/public", express.static("public"));
-app.put("update-task", (req, res) => {
-  const taskId = req.body.taskId;
-  const newTask = req.body.newTask;
+//タスク編集画面遷移
+app.get("/edit/:id", (req, res) => {
+  const taskId = req.params.id;
+  //idに一致するタスクをfindで取得する
+  const task = tasks.find((task) => task.id === +taskId);
 
-  //idが一致する要素のインデックスを取得
-  const index = tasks.findIndex((el) => el.id === +taskId);
-  //インデックスの要素を更新
-  tasks.splice(index, 1);
+  if (task === undefined) {
+    //idが一致しない場合indexに遷移
+    res.redirect("/");
+  }
 
-  res.render("index", { tasks: tasks });
+  res.render("edit", { task: task });
+});
+
+//タスク更新処理
+app.post("/update/:id", (req, res) => {
+  const id = req.params.id;
+  const newTitle = req.body.task;
+
+  //更新するタスクIDが一致するまでループ
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === +id) {
+      tasks[i].title = newTitle;
+      break;
+    }
+  }
+
+  res.redirect("/");
 });
